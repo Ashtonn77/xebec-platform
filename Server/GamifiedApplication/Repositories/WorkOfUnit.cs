2fc33782fa565912aaeb4f2044fe64b3230f4e41
Tshegofatso <Tshegofatso@LAP-JHB-005>
2021-10-11 09:25:15 +0200

    added oauth backend

---
+++ Server/GamifiedApplication/Repositories/WorkOfUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XebecPortal.Shared.GamifiedAplication;
+
+namespace Server.GamifiedAplication
+{
+    public class WorkOfUnit : IWorkOfUnit
+    {
+        private readonly AppDbContext _context;
+
+        private IEncompassingRepository<AppUser> _appusers;
+        public WorkOfUnit(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEncompassingRepository<AppUser> AppUsers => _appusers ??= new EncompassingRepository<AppUser>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
