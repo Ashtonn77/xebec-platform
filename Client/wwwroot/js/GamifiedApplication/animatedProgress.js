3cbb5bc33617e0e7fa64c158d56bb25609ab334a
Ash <ashton.naidoo@1nebula.com>
2021-10-18 16:51:10 +0200

    update google oauth

--- Client/wwwroot/js/GamifiedApplication/animatedProgress.js
+++ Client/wwwroot/js/GamifiedApplication/animatedProgress.js
@@ -1,12 +1,47 @@
 var progress;
+var personal;
+var education;
+var work;
+var additional;
 
 function initializeProgress()
 {
     progress = document.getElementById('progress');
+    personal = document.getElementById('personal');
+    education = document.getElementById('education');
+    work = document.getElementById('work');
+    additional = document.getElementById('additional');    
 }
 
 
-function toggleProgress(x)
+function toggleProgress(x, icon)
 {
-    progress.style.width = progress.style.width + x + '%';    
+    progress.style.width = x + '%';
+
+    if (icon == "personal")
+    {
+        personal.style.color = 'green';
+        education.style.color = 'black';
+        work.style.color = 'black';
+        additional.style.color = 'black';
+    }
+    else if (icon == "education") {
+        personal.style.color = 'black';
+        education.style.color = 'green';
+        work.style.color = 'black';
+        additional.style.color = 'black';
+    }
+    else if (icon == "work") {
+        personal.style.color = 'black';
+        education.style.color = 'black';
+        work.style.color = 'green';
+        additional.style.color = 'black';
+    }
+    else if (icon == "additional") {
+        personal.style.color = 'black';
+        education.style.color = 'black';
+        work.style.color = 'black';
+        additional.style.color = 'green';
+    }   
+
 }
