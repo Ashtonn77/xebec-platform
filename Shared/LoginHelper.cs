bdcb087e1199bd0d6716ea7c857684d43cab7150
Ash <ashton.naidoo@1nebula.com>
2021-10-29 12:03:41 +0200

    test

--- Shared/LoginHelper.cs
+++ Shared/LoginHelper.cs
@@ -12,7 +12,6 @@ namespace XebecPortal.Shared
         public int Id { get; set; }
         public DateTime TimeDateLogin { get; set; }
         public DateTime TimeDateLogOut { get; set; } 
-
         //foreign key
         public int AppUserId { get; set; }
 
