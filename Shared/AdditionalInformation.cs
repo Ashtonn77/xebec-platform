26f96b39b0589d191dea720f1862ca20b0a71f32
KianChetty <kian10.chetty@gmail.com>
2021-11-05 11:30:47 +0200

    ProfilePortfolio API

--- Shared/AdditionalInformation.cs
+++ Shared/AdditionalInformation.cs
@@ -10,13 +10,12 @@ namespace XebecPortal.Shared
     {
         public int Id { get; set; }
 
-        public string GitHubLink { get; set; }
+        public string Disability { get; set; }
 
-        public string LinkedInLink { get; set; }
+        public string Gender { get; set; }
 
-        public string FacebookLink { get; set; }
+        public string Ethnicity { get; set; }
 
-        public string PersonalWebsiteUrl { get; set; }
 
         //foreign key
         public int AppUserId { get; set; }
