5b4ea02d30f7c06be1d5539f18c94b08af481dd0
stfeleti <stfeleti@gmail.com>
2021-11-17 11:02:03 +0200

    Promise, Everything works Please don't lose this.

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using XebecPortal.Client.GamifiedEnvBeta.Utils;
+using XebecPortal.Client.JobPortalTestEnv.New_Job_Board;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using XebecPortal.Client.JobPortalTestEnv;
 
 namespace XebecPortal.Client
 {
@@ -21,7 +24,14 @@ namespace XebecPortal.Client
             builder.Services.AddSingleton<State>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<NotifierService>();
 
+
+            builder.Services.AddScoped<IPersonalInformationDataService, PersonalInformationDataService>();
+            builder.Services.AddScoped<IApplicationPhaseDataService, ApplicationPhaseDataService>();
+            builder.Services.AddScoped<IApplicationPhaseHelperDataService, ApplicationPhaseHelperDataService>();
+            builder.Services.AddScoped<IStatusDataService, StatusDataService>();
+            builder.Services.AddScoped<IJobDataService, JobDataService>();
             await builder.Build().RunAsync();
         }
     }
