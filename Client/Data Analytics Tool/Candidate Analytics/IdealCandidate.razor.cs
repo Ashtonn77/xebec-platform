5572306f9dab81cd0c80a5c113ad350b0f930b4e
stfeleti <stfeleti@gmail.com>
2021-11-22 15:34:56 +0200

    added candidate page

---
+++ Client/Data Analytics Tool/Candidate Analytics/IdealCandidate.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XebecPortal.Client.Data_Analytics_Tool.Candidate_Analytics
+{
+    public partial class IdealCandidate : ComponentBase
+    {
+        [Parameter]
+        public int DepartmentId { get; set; }
+        [Parameter]
+        public int JobId { get; set; }
+    }
+}
