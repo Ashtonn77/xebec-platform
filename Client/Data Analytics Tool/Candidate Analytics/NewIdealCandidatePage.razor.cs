f0f246c8c6e3599f37e9bfc75e87a73f196e69e7
stfeleti <stfeleti@gmail.com>
2021-11-25 12:36:45 +0200

    Added new Ideal Candidate page,  display section and popup form

--- Client/Data Analytics Tool/Candidate Analytics/NewIdealCandidatePage.razor.cs
+++ Client/Data Analytics Tool/Candidate Analytics/NewIdealCandidatePage.razor.cs
@@ -5,32 +5,24 @@ using System.Linq;
 
 namespace XebecPortal.Client.Data_Analytics_Tool.Candidate_Analytics
 {
-    public partial class IdealCandidate : ComponentBase
+    public partial class NewIdealCandidatePage : ComponentBase
     {
-        [Parameter]
-        public int DepartmentId { get; set; }
-        [Parameter]
-        public int JobId { get; set; }
-
+        public string PopupDisplay { get; set; } = "block";
+        
         public int GlobalMax { get; set; } = 100;
-        public int GlobalMin {get; set; } = 0;
-        
-        public int SalaryImportance {get; set;} = 0;
+        public int GlobalMin { get; set; } = 0;
+
+        public int SalaryImportance { get; set; } = 0;
         public int StartDateImportance { get; set; } = 0;
-        
-        public int NoticeImportance {get; set;} = 0;
+
+        public int NoticeImportance { get; set; } = 0;
         public int ExperienceImportance { get; set; } = 0;
-        
+
         public int UniversityImportance { get; set; } = 0;
         public int PlatformImportance { get; set; } = 0;
-        
+
         public int CitizenshipImportance { get; set; } = 0;
         public int VisaImportance { get; set; } = 0;
         public int PermitImportance { get; set; } = 0;
-
-        private void UpdateMax(int importance)
-        {
-            GlobalMax -= importance;
-        }
     }
 }
